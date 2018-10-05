fc11503d97cc7dec8b1bfda9609069ff1efbbceb
Mariano Casco <marianoc88@gmail.com>
2018-10-04 22:23:00 -0300

    Create rkx binary for repeat-key xor encryption

--- src/bin/rkx.rs
+++ src/bin/rkx.rs
@@ -1,4 +1,21 @@
 /// Repeated-key xor
 extern crate cryptopals;
 
-fn main() {}
+use std::env;
+use std::io::{self, Read};
+
+use cryptopals::{build_repeated_key, Bytes};
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("wrong args");
+        return;
+    }
+    let key = &args[1];
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let bytes = Bytes::from_str(&buffer);
+    let repkey = Bytes::from_str(&build_repeated_key(key, bytes.len()));
+    println!("{}", bytes.xor(&repkey).hex_encode());
+}
