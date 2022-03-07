95bd4215e4c127c1a82fc2f53f64700c92c9aaa3
Mariano Casco <madasbytes@gmail.com>
2022-03-06 22:16:22 -0300

    Rename challenge 2-1 to 2-9

--- src/set2.rs
+++ src/set2.rs
@@ -2,12 +2,12 @@
 
 use cryptopals::{pad_block};
 
-fn challenge1() {
+fn challenge9() {
     let b = b"YELLOW SUBMARINE";
     let padded = pad_block(b, 20);
     assert_eq!(&padded, b"YELLOW SUBMARINE\x04\x04\x04\x04");
 }
 
 fn main() {
-    challenge1();
+    challenge9();
 }
